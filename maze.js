652c62735e75767eaa02e18298f7cf27794d01ab
xovest <79008917+xovest@users.noreply.github.com>
2021-11-06 17:19:04 +0200

    works like damn

--- maze.js
+++ maze.js
@@ -27,7 +27,7 @@ class Maze {
   draw() {
     maze.width = this.size;
     maze.height = this.size;
-    maze.style.background = 'black'; 
+    maze.style.background = 'green'; 
     cur.visited = true;
     
     for (let r = 0; r < this.rows; ++r) {
@@ -36,6 +36,29 @@ class Maze {
         grid[r][c].show(this.size, this.rows, this.cols);
       }
     }
+
+    let next = cur.checkNeighbours();
+    if (next) {
+      next.visited = true;
+
+      this.stack.push(cur);
+
+      cur.highlight(this.cols);
+      cur.removeWall(cur, next);
+      cur = next;
+    } else if (this.stack.length > 0) {
+      let cell = this.stack.pop();
+      cur = cell;
+      cur.highlight(this.cols);
+    }
+
+    if (this.stack.length == 0) {
+      return;
+    }
+
+    window.requestAnimationFrame(() => {
+      this.draw();
+    })
   }
 }
 
@@ -61,7 +84,7 @@ class Cell {
     let neighbours = [];
 
     let top = row !== 0 ? grid[row - 1][col] : undefined;
-    let right = col !== grid.length - 1 ? grid[row - 1][col + 1] : undefined;
+    let right = col !== grid.length - 1 ? grid[row][col + 1] : undefined;
     let bottom = row !== grid.length - 1 ? grid[row + 1][col] : undefined;
     let left = col !== 0 ? grid[row][col - 1] : undefined;
 
@@ -72,6 +95,7 @@ class Cell {
 
     if (neighbours.length !== 0) {
       let random = Math.floor(Math.random() * neighbours.length);
+      return neighbours[random];
     } else {
       return undefined;
     }
@@ -105,12 +129,41 @@ class Cell {
     ctx.stroke();
   }
 
+  highlight(cols) {
+    let x = this.colNum * this.parentSize / cols + 1;
+    let y = this.rowNum * this.parentSize / cols + 1;
+
+    ctx.fillStyle = 'pink';
+    ctx.fillRect(x, y, this.parentSize / cols - 3, this.parentSize / cols - 3);
+  }
+
+  removeWall(c1, c2) {
+    let x = c1.colNum - c2.colNum;
+
+    if (x == 1) {
+      c1.walls.leftWall = false;
+      c2.walls.rightWall = false;
+    } else if (x == -1) {
+      c1.walls.rightWall = false;
+      c2.walls.leftWall = false;
+    }
+
+    let y = c1.rowNum - c2.rowNum;
+    if (y == 1) {
+      c1.walls.topWall = false;
+      c2.walls.bottomWall = false;
+    } else if (y == -1) {
+      c1.walls.bottomWall = false;
+      c2.walls.topWall = false;
+    }
+  }
+
   show(size, rows, cols) {
     let x = this.colNum * size / cols;
     let y = this.rowNum * size / rows;
 
     ctx.strokeStyle = 'white';
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = 'green';
     ctx.lineWidth = 2;
 
     if (this.walls.topWall) this.drawTopWall(x, y, size, cols, rows);
@@ -123,6 +176,6 @@ class Cell {
   }
 }
 
-let newMaze = new Maze(500, 10, 10);
+let newMaze = new Maze(500, 20, 20);
 newMaze.setup();
 newMaze.draw();
